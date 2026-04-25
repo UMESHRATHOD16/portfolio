faf36ef8e5e9c51596fdb930b9292bcbbf9bac6a
UMESHRATHOD16 <umeshhhhhrathod@gmail.com>
2026-04-25 20:36:13 +0530

    setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import MainPage from './pages/MainPage'
+import AboutChapter from './pages/AboutChapter'
+import SkillsChapter from './pages/SkillsChapter'
+import ProjectsChapter from './pages/ProjectsChapter'
+import ContactChapter from './pages/ContactChapter'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>Hello World</h1>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/about" element={<AboutChapter />} />
+      <Route path="/skills" element={<SkillsChapter />} />
+      <Route path="/projects" element={<ProjectsChapter />} />
+      <Route path="/contact" element={<ContactChapter />} />
+    </Routes>
   )
 }
 
