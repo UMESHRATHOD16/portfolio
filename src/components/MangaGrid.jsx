0401d15b6174f2237e84a1f7ccdfc89948f88a7e
UMESHRATHOD16 <umeshhhhhrathod@gmail.com>
2026-05-02 11:22:16 +0530

    feat : added about section

--- src/components/MangaGrid.jsx
+++ src/components/MangaGrid.jsx
@@ -3,56 +3,56 @@ import './MangaGrid.css'
 function MangaGrid() {
   return (
     <div className="manga-grid">
-        <div className="panel hero-panel"><div className="panel hero-panel">
+        <div className="panel hero-panel">
         <span className="hero-chapter">第01話</span>
         <h1 className="hero-name">UMESH<br/>RATHOD</h1>
         <p className="hero-sub">IT Student · NIT Jalandhar · Web Developer</p>
-        </div>
     </div>
 
       {/* about */}
-      <div className="panel about-panel"><div className="panel about-panel">
+      <div className="panel about-panel">
         <span className="panel-label">第02話 · ABOUT</span>
-        <p>Origin Story</p>
+        <div className="about-content">
+          <p className="about-intro">Rathod Umesh. 21</p>
+          <p className="about-line">Started with GTA on a friend's PC.<br/>Ended up building AI-powered web apps.</p>
+          <p className="about-accent">Not a bad arc.</p>
+         <p className="about-line">
+            Still in training arc.<br/>
+            React, Node.js, DSA - mastering each arc.<br/>
+            Power level : rising.
+        </p>
+          <p className="about-sign">Luffy never stops until he's King of the Pirates.<br/>I don't stop either.</p>
         </div>
       </div>
 
       {/* Skills */}
         <div className="panel skills-panel">
-          <div className="panel about-panel">
           <span className="panel-label">第03話 · SKILLS</span>
           <p>Origin Story</p>
-          </div>
         </div>
 
+      {/* Projects Header */}
+      <div className="panel projects-header-panel">
+        <span className="panel-label"></span>
+        <h2 className=" panel-label panel-title">第04話 · WHAT I'VE BUILT</h2>
+      </div>
 
       {/* Project1 */}
         <div className="panel project1-panel">
-          <div className="panel about-panel">
-          <span className="panel-label">第04話 · WHAT IV'E BUILT</span>
-          <p>Origin Story</p>
-          </div>
+          <p>Mega Blog</p>
         </div>
 
 
       {/* Project2 */}
         <div className="panel project2-panel">
-          P2
+          <p>EcoSangam</p>
         </div>
 
 
-      {/* Project3 */}
-        <div className="panel project3-panel">
-          P3
-        </div>
-
-        
       {/* Contact */}
         <div className="panel contact-panel">
-          <div className="panel about-panel">
           <span className="panel-label">第05話 · LET'S CONNECT</span>
           <p>Origin Story</p>
-          </div>
         </div>
     </div>
   )
