bed9eea399cc87aae458f0562f8bbcbdc73e0816
UMESHRATHOD16 <umeshhhhhrathod@gmail.com>
2026-05-16 17:13:50 +0530

    demo about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,4 @@
-import React from 'react'
-
 function About() {
-  return (
-    <div>About</div>
-  )
+  return <div style={{ background: '#0a0a0a', color: 'white', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', fontFamily: 'Bangers' , fontSize: '4rem' }}>ABOUT CHAPTER — COMING SOON</div>
 }
-
 export default About
